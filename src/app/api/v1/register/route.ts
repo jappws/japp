33d782bb1965379351dc92dfef8b21240e0c5b00
Kahindo Lwanzo Alfred <89693650+alfredlw@users.noreply.github.com>
2023-11-27 14:49:03 +0200

    deploy changes

--- src/app/api/v1/register/route.ts
+++ src/app/api/v1/register/route.ts
@@ -47,8 +47,8 @@ export async function POST(request: Request) {
 
     //generate a user code as a username
     const currentYear = new Date().getFullYear();
-    const yearSuffix = String(currentYear).substring(-2);
-    const randomPrefix = String(Math.floor(Math.random() * 100)).padStart(
+    const yearSuffix = String(currentYear).substring(2);
+    const randomPrefix = String(Math.floor(Math.random() * 1000)).padStart(
       3,
       "0"
     );
