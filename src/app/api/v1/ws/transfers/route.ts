2e73f1772e611d36f4445418244a27a9984bd089
Kahindo Lwanzo Alfred <89693650+alfredlw@users.noreply.github.com>
2023-11-29 19:29:06 +0200

    deploy sdafhsjdaf

--- src/app/api/v1/ws/transfers/route.ts
+++ src/app/api/v1/ws/transfers/route.ts
@@ -5,6 +5,7 @@ export async function GET(request: Request) {
   try {
     const transfers = await prisma.transfer.findMany({
       orderBy: { date: "desc" },
+      include: { operator: {} },
     });
 
     const balance = await prisma.transferBalance.findUnique({
