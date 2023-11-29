d47d16e90d5f417f9231f2ace3c404b361a7998a
Kahindo Lwanzo Alfred <89693650+alfredlw@users.noreply.github.com>
2023-11-29 19:21:06 +0200

    deploy chansdjfsa

--- src/components/ws/transfersAndCredits/transactions/list.tsx
+++ src/components/ws/transfersAndCredits/transactions/list.tsx
@@ -3,31 +3,49 @@
 import { TransferType } from "@/lib/types";
 import { Table } from "antd";
 import { transfersColumns } from "./columns";
-import React from "react";
+import React, { useState } from "react";
+import { SelectedTransferRightSider } from "./transferRightSider";
+import { cn } from "@/lib/utils";
 
 type Props = {
   data?: TransferType[];
-  isLoading:boolean
+  isLoading: boolean;
 };
 
-export const TransfersList: React.FC<Props> = ({ data,isLoading }) => {
-  const onRowClick = (record: TransferType) => {};
+export const TransfersList: React.FC<Props> = ({ data, isLoading }) => {
+  const [selectedTransfer, setSelectedTransfer] = useState<TransferType>();
+  const [openSelectedTransferSider, setOpenSelectedTransferSider] =
+    useState<boolean>(false);
+
+  const onRowClick = (record: TransferType) => {
+    setOpenSelectedTransferSider(true);
+    setSelectedTransfer(record);
+  };
 
   return (
-    <Table
-      loading={isLoading}
-      rowClassName={(rowData) => "bg-[#f5f5f5] odd:bg-white hover:cursor-pointer"}
-      columns={transfersColumns}
-      dataSource={data}
-      size="small"
-      pagination={{ defaultPageSize: 50, pageSizeOptions: [25, 50, 75, 100] }}
-      tableLayout="fixed"
-      bordered={true}
-      onRow={(record) => {
-        return {
-          onClick: onRowClick.bind(this, record), // click row
-        };
-      }}
-    />
+    <>
+      <Table
+        loading={isLoading}
+        rowClassName={(rowData) =>
+          cn("bg-[#f5f5f5] odd:bg-white hover:cursor-pointer", selectedTransfer?.id===rowData.id && "bg-primary bg-opacity-25")
+        }
+        columns={transfersColumns}
+        dataSource={data}
+        size="small"
+        pagination={{ defaultPageSize: 50, pageSizeOptions: [25, 50, 75, 100] }}
+        tableLayout="fixed"
+        bordered={true}
+        onRow={(record) => {
+          return {
+            onClick: onRowClick.bind(this, record), // click row
+          };
+        }}
+      />
+      <SelectedTransferRightSider
+        open={openSelectedTransferSider}
+        trigger={setOpenSelectedTransferSider}
+        data={selectedTransfer}
+      />
+    </>
   );
 };
