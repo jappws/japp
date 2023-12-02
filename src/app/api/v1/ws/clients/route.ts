611421e6b4e7703c287adf4dc68750e13ebbb5fe
Kahindo Lwanzo Alfred <89693650+alfredlw@users.noreply.github.com>
2023-12-02 19:20:32 +0200

    deploy changes

--- src/app/api/v1/ws/clients/route.ts
+++ src/app/api/v1/ws/clients/route.ts
@@ -1,6 +1,8 @@
 import prisma from "@/lib/prisma";
 import { Prisma } from "@prisma/client";
 
+export const dynamic = 'force-dynamic'
+
 export async function GET(request: Request, { params }: { params: {} }) {
   try {
     const accounts = await prisma.account.findMany({
