46567e525b91c22a0cf8048631abc81d6b89de7e
Kahindo Lwanzo Alfred <89693650+alfredlw@users.noreply.github.com>
2023-12-04 16:39:21 +0200

    deploy achensadjhf

---
+++ src/app/api/v1/ws/dashboard/route.ts
@@ -0,0 +1,58 @@
+import prisma from "@/lib/prisma";
+import { Prisma } from "@prisma/client";
+
+// export const dynamic = 'force-dynamic'
+
+export async function GET(request: Request, { params }: { params: {} }) {
+  try {
+
+    const transfersBalance = await prisma.transferBalance.findUnique({
+        where: { id:1 },
+      });
+
+      const bankAggregations = await prisma.account.aggregate({
+        where: { owner: { deleted: false } },
+        _avg: { balance: true },
+        _max: { balance: true },
+        _min: { balance: true },
+        _sum: { balance: true },
+        _count: true,
+      });
+
+    const res = {
+      banckAndCredits: {
+        numberOfAccounts: bankAggregations._count,
+        maxBalance: bankAggregations._max.balance,
+        minBalance: bankAggregations._min.balance,
+        totalBalance: bankAggregations._sum,
+      },
+      transferAndCredits: {
+        balance:transfersBalance
+      },
+    };
+    return new Response(JSON.stringify(res), { status: 200 });
+  } catch (e: any) {
+    //Tracking prisma error
+    if (e instanceof Prisma.PrismaClientKnownRequestError) {
+      const error_response = {
+        status: "fail",
+        code: e.code,
+        message: e.message,
+        clientVersion: e.clientVersion,
+      };
+      return new Response(JSON.stringify(error_response), {
+        status: 400,
+        headers: { "Content-Type": "application/json" },
+      });
+    }
+    // tracking other internal server
+    const error_response = {
+      status: "error",
+      message: e.message,
+    };
+    return new Response(JSON.stringify(error_response), {
+      status: 500,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+}
--- src/components/ws/dashboard/index.tsx
+++ src/components/ws/dashboard/index.tsx
@@ -1,9 +1,31 @@
-'use client'
+"use client";
 
 import { DashboardOutlined } from "@ant-design/icons";
 import { PageContainer, ProCard } from "@ant-design/pro-components";
+import { TransferBalance } from "@prisma/client";
+import { useQuery } from "@tanstack/react-query";
+import { Button } from "antd";
+import axios from "axios";
 
 export const DashboardClient = () => {
+  const { data: account, isLoading } = useQuery({
+    queryKey: ["dashboard"],
+    queryFn: () =>
+      axios.get(`/api/v1/ws/dashboard`).then(
+        (res) =>
+          res.data as {
+            banckAndCredits: {
+              numberOfAccounts: number;
+              maxBalance: number | null;
+              minBalance: number | null;
+              totalBalance: number | null;
+            };
+            transferAndCredits: {
+              balance: TransferBalance | null;
+            };
+          }
+      ),
+  });
   return (
     <div>
       <PageContainer
@@ -22,10 +44,14 @@ export const DashboardClient = () => {
       >
         <div className="md:pt-4">
           <ProCard
-            style={{
-              height: "200vh",
-              minHeight: 800,
-            }}
+            title="Banque et crédits"
+            style={{ marginBlockEnd: 16 }}
+            extra={[<Button key="1">Voir plus</Button>]}
+          ></ProCard>
+          <ProCard
+            title="Transferts et expéditions"
+            style={{ marginBlockEnd: 16 }}
+            extra={[<Button key="1">Voir plus</Button>]}
           ></ProCard>
         </div>
       </PageContainer>
