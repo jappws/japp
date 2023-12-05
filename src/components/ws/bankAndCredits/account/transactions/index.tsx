8b0967a9cb17c5cf716670b75d90367dee63204a
Kahindo Lwanzo Alfred <89693650+alfredlw@users.noreply.github.com>
2023-12-05 07:21:35 +0200

    piumsdajf adf

--- src/components/ws/bankAndCredits/account/transactions/index.tsx
+++ src/components/ws/bankAndCredits/account/transactions/index.tsx
@@ -153,7 +153,6 @@ export const AccountTransactions = () => {
               "DDMMYYYYHHmmss"
             )}`}
           />
-          ,
         </Space>
       </header>
       <TransactionsList data={selectedCurrentData} isLoading={isLoading} />
