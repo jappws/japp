8d8dd52fee2b332d726b79f2a251f03af160f595
Kahindo Lwanzo Alfred <89693650+alfredlw@users.noreply.github.com>
2024-01-31 13:13:25 +0200

    go

--- src/components/ws/bankAndCredits/accounts/forms/deleteAccountForm.tsx
+++ src/components/ws/bankAndCredits/accounts/forms/deleteAccountForm.tsx
@@ -114,7 +114,7 @@ export const DeleteAccountForm: React.FC<Props> = ({
         disabled={isPending}
       >
         <div className="bg-white">
-          <div className="flex space-x-3 p-3 bg-red-50 mb-5 rounded-sm">
+          <div className="flex space-x-3 p-3 bg-red-50 mb-5 rounded-md">
             <WarningFilled className="text-red-500 text-6xl" />
             <div className="flex-1">
               Le compte sera définitivement supprimé, y compris ses mouvements
