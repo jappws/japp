4009d6eb6050fd89da3ec43ebce79e1b2cf74f5b
Kahindo Lwanzo Alfred <89693650+alfredlw@users.noreply.github.com>
2024-02-02 12:28:59 +0200

    deployer

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
  
 export default function NotFound() {
   return (
-    <div className='flex justify-center items-center'>
+    <div className=' min-h-screen flex flex-col justify-center items-center'>
       <h2>404: Page non trouvé</h2>
       <p>Impossible de trouver la ressource demandée</p>
       <Link href="/">Rentrer à la maison</Link>
