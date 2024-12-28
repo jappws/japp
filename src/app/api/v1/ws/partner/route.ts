ab7eda48968c27f52cc23c05cb00225337cf9587
Kahindo Lwanzo Alfred <89693650+alfredlwanzo@users.noreply.github.com>
2024-12-28 06:19:14 +0200

    deploy changes

--- src/app/api/v1/ws/partner/route.ts
+++ src/app/api/v1/ws/partner/route.ts
@@ -1,46 +1,117 @@
+// import prisma from "@/lib/prisma";
+// import { Prisma } from "@prisma/client";
+
+// type BodyRequestType = {
+//   code: string;
+// };
+
+// export async function POST(request: Request) {
+//   try {
+//     const body: BodyRequestType = await request.json();
+//     const createdPartner = await prisma.partner.create({
+//       data: {
+//         code: body.code,
+//         balance: 0,
+//       },
+//     });
+
+//     const res = {
+//       ...createdPartner,
+//     };
+//     return new Response(JSON.stringify(res), { status: 201 });
+//   } catch (e: any) {
+//     //Tracking prisma error
+//     if (e instanceof Prisma.PrismaClientKnownRequestError) {
+//       const error_response = {
+//         status: "fail",
+//         code: e.code,
+//         message: e.message,
+//         clientVersion: e.clientVersion,
+//       };
+//       return new Response(JSON.stringify(error_response), {
+//         status: 400,
+//         headers: { "Content-Type": "application/json" },
+//       });
+//     }
+//     // tracking other internal server
+//     const error_response = {
+//       status: "error",
+//       message: e.message,
+//     };
+//     return new Response(JSON.stringify(error_response), {
+//       status: 500,
+//       headers: { "Content-Type": "application/json" },
+//     });
+//   }
+// }
+
 import prisma from "@/lib/prisma";
 import { Prisma } from "@prisma/client";
+import { type NextRequest } from "next/server";
 
-type BodyRequestType = {
-  code: string;
-};
+// ///////////////////////////////////////////////////////////////
+// //////////// Récupérer les transferts et le partenaire /////////
+// //////////////////////////////////////////////////////////////
 
-export async function POST(request: Request) {
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { partnerId: string } }
+) {
   try {
-    const body: BodyRequestType = await request.json();
-    const createdPartner = await prisma.partner.create({
-      data: {
-        code: body.code,
-        balance: 0,
-      },
+    // Récupérer l'ID du partenaire à partir des paramètres
+    const { partnerId } = await params;
+
+    // Récupérer les transferts du partenaire
+    const transfers = await prisma.transfer.findMany({
+      where: { partnerId: Number(partnerId) },
+      orderBy: { date: "asc" },
+      include: { operator: true },
     });
 
-    const res = {
-      ...createdPartner,
-    };
-    return new Response(JSON.stringify(res), { status: 201 });
-  } catch (e: any) {
-    //Tracking prisma error
-    if (e instanceof Prisma.PrismaClientKnownRequestError) {
-      const error_response = {
-        status: "fail",
-        code: e.code,
-        message: e.message,
-        clientVersion: e.clientVersion,
-      };
-      return new Response(JSON.stringify(error_response), {
-        status: 400,
-        headers: { "Content-Type": "application/json" },
-      });
-    }
-    // tracking other internal server
-    const error_response = {
-      status: "error",
-      message: e.message,
-    };
-    return new Response(JSON.stringify(error_response), {
-      status: 500,
-      headers: { "Content-Type": "application/json" },
+    // Récupérer les informations du partenaire
+    const partner = await prisma.partner.findUnique({
+      where: { id: Number(partnerId) },
     });
+
+    // Ajouter le solde après chaque transfert
+    let balanceAfter = 0.0;
+    const transfersWithBalanceAfter = transfers.map((transfer) => {
+      // Calculer le solde après le transfert
+      if (transfer.type === "GOLD_TRANSFER") {
+        balanceAfter += transfer.amount;
+
+      } else if (transfer.type === "MONEY_TRANSFER") {
+        balanceAfter -= transfer.amount;
+      }
+      return {
+        ...transfer,
+        balanceAfter: balanceAfter,
+      }
+    });
+
+    // Combiner les résultats en un seul objet de réponse
+    const res = { partner, transfers: transfersWithBalanceAfter };
+
+    // Retourner la réponse avec les transferts et les informations du partenaire
+    return new Response(JSON.stringify(res), { status: 200 });
+  } catch (e: any) {
+    return handleError(e);
   }
 }
+
+// Fonction de gestion des erreurs
+function handleError(e: any) {
+  const error_response = {
+    status: e instanceof Prisma.PrismaClientKnownRequestError ? "fail" : "error",
+    code: e.code,
+    message: e.message,
+    clientVersion: e.clientVersion,
+  };
+
+  // Retourner la réponse d'erreur
+  return new Response(JSON.stringify(error_response), {
+    status: e instanceof Prisma.PrismaClientKnownRequestError ? 400 : 500,
+    headers: { "Content-Type": "application/json" },
+  });
+}
+
